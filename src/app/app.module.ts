fbd943d1f428875760fc71aa09da8e3f33d5d4b2
juargo <jorgeretamalaburto@gmail.com>
2018-06-02 22:38:50 -0700

    form template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GameFormComponent } from './game-form/game-form.component';
@@ -12,7 +12,8 @@ import { GameFormComponent } from './game-form/game-form.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
